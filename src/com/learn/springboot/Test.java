2312068850b717190d617546dfd9e216804bd8b9
Raul7Madrid <yaoyuan23@qq.com>
2019-05-14 23:21:14 +0800

    First Commit

---
+++ src/com/learn/springboot/Test.java
@@ -0,0 +1,15 @@
+package com.learn.springboot;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String name1 = "张三";
+		String name2 = "李四";
+		if(name1.equals(name2)) {
+			System.out.println(name1);
+		}else{
+			System.out.println(name2);
+		}
+	}
+
+}
